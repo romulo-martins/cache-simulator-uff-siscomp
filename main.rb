1050c7065674d5fc506891816a513d0b52698900
romuluxs <romuluxs@gmail.com>
2018-09-19 22:41:20 -0300

    Implementando o algoritmo LRU.

--- main.rb
+++ main.rb
@@ -22,10 +22,30 @@ def fifo(cache, mem_refs)
 end	
 
 def lru(cache, mem_refs)
-	# TODO: 
+	misses = 0
+	hits = 0
+	count = [0] * cache.size
+	mem_refs.each do |value|
+		if cache.include?(value) 
+			hits += 1
+			value_index = cache.index(value)
+			count[value_index] = 0
+		else
+			misses += 1
+			if cache.include?(-1)
+				cache[cache.index(-1)] = value
+			else
+				count_index = count.index(count.max)
+				cache[count_index] = value
+				count[count_index] = 0
+			end
+		end
+		count.map! { |c| c += 1 }
+		print_cache(cache, misses, hits)	
+	end
 end	
 
-def lfu(cahce, mem_refs)
+def lfu(cache, mem_refs)
 	# TODO: 
 end
 
@@ -55,7 +75,7 @@ if ARGV.include?('--size')
 	cache_size = ARGV[value_index].to_i
 end
 
-algorithm = 1
+algorithm = ''
 if ARGV.include?('--algorithm')
 	value_index = ARGV.index('--algorithm') + 1
 	algorithm = ARGV[value_index]
@@ -64,8 +84,10 @@ end
 mem_refs = File.readlines(path).map(&:to_i)
 cache = [-1] * cache_size
 
-if algorithm == 'FIFO'
+if algorithm.upcase == 'FIFO'
 	fifo(cache, mem_refs)
+elsif algorithm.upcase == 'LRU'
+	lru(cache, mem_refs)
 else
 	puts 'Error: Algoritmo invalido!'
 end
